02065bf9c2386828d4a377936dd625ae859b0562
THIAGO TEIXEIRA CEZAR <thiago.tcezar@senacsp.edu.br>
2024-09-26 22:07:31 -0300

    feat: entrada, processamento e saída de dados

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Entrada de dados
+let num1 = parseFloat(prompt("Digite o primeiro número:"));
+let num2 = parseFloat(prompt("Digite o segundo número:"));
+
+// Processamento
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let exponenciacao = num1 ** num2;
+let modulo = num1 % num2;
+
+// Saída de dados
+alert("A soma é: " + soma);
+alert("A subtração é: " + subtracao);
+alert("A multiplicação é: " + multiplicacao);
+alert("A divisão é: " + divisao);
+alert("A exponenciacao de num1 a num2 é: " + exponenciacao);
+alert("O resto da divisão é: " + modulo);
