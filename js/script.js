b54044511c498e1b655860144ca9329656969d10
thiago.tcezar@senacsp.edu.br <@tt875524>
2024-09-26 22:17:23 -0300

    feat: adicionando operadores lógicos

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@ alert("A multiplicação é: " + multiplicacao);
 alert("A divisão é: " + divisao);
 alert("A exponenciacao de num1 a num2 é: " + exponenciacao);
 alert("O resto da divisão é: " + modulo);
+
+let a = 10;
+let b = 20;
+ 
+let ehIgual = a == b;
+let ehDiferente = a != b;
+let ehEstritIgual = a === b;
+let ehEstritDiferente = a !== b;
+let ehMaior = a > b;
+let ehMenorOuIgual = a <= b;
+let ehMaiorOuIgual = a >= b;
+ 
+let naoEhIgual = !ehIgual;
+let naoEhDiferente_EhMenor_ou_EhMaiorOuIgual = !ehDiferente && (ehMenor || ehMaiorOuIgual);
+ 
+console.log("É igual: " + ehIgual);
+console.log("É diferente: " + ehDiferente);
+console.log("É Estritamente Igual: " + ehEstritIgual);
+console.log("É Estritamente Diferente: " + ehEstritDiferente);
+console.log("É Maior Ou Igual: " + ehMaiorOuIgual);
+console.log("É Menor Ou Igual: " + ehMenorOuIgual);
+console.log("Não É igual: " + naoEhIgual);
+console.log("Não É Diferente e É Menor ou É Maior Ou Igual: " + naoEhDiferente_EhMenor_ou_EhMaiorOuIgual);
